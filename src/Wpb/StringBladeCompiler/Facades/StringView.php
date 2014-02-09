583bd6d84ca373b24ee58387b9f2f1abef704c00
Terre Porter <tporter@webpage-builders.com>
2014-02-09 16:17:31 -0500

    Initial Upload

---
+++ src/Wpb/StringBladeCompiler/Facades/StringView.php
@@ -0,0 +1,13 @@
+<?php namespace Wpb\StringBladeCompiler\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class StringView extends Facade {
+
+	/**
+	 * Get the registered name of the component.
+	 *
+	 * @return string
+	 */
+	protected static function getFacadeAccessor() { return 'stringview'; }
+}
---
+++ src/Wpb/StringBladeCompiler/StringBladeCompilerServiceProvider.php
@@ -0,0 +1,53 @@
+<?php namespace Wpb\StringBladeCompiler;
+
+use Illuminate\Support\ServiceProvider;
+
+class StringBladeCompilerServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('wpb/string-blade-compiler');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app->bind('stringview', 'Wpb\StringBladeCompiler\StringView');
+
+                /*
+               * This removes the need to add a facade in the config\app
+               */
+                $this->app->booting(function()
+                {
+                    $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+                    $loader->alias('StringView', 'Wpb\StringBladeCompiler\Facades\StringView');
+                });
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
