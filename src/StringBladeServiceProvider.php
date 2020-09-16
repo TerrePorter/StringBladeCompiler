0b556742935fda99b3fcd20e3914568ca03e5db1
vagrant <vagrant@hs1112c>
2020-09-16 13:17:50 +0000

    Update for laravel 8

--- src/StringBladeServiceProvider.php
+++ src/StringBladeServiceProvider.php
@@ -18,20 +18,16 @@ class StringBladeServiceProvider extends ViewServiceProvider implements Deferrab
      */
     public function register()
     {
-
         // include the package config
         $this->mergeConfigFrom(
             __DIR__.'/../config/blade.php', 'blade'
         );
 
-        // load the alias (handled by the Laravel autoloader)
-        //$this->app->alias('StringBlade', 'Wpb\String_Blade_Compiler\Facades\StringBlade');
-
+        $this->registerFactory();
+        $this->registerViewFinder();
+        $this->registerBladeCompiler();
         $this->registerEngineResolver();
 
-        $this->registerViewFinder();
-
-        $this->registerFactory();
     }
 
     /**
@@ -176,6 +172,10 @@ class StringBladeServiceProvider extends ViewServiceProvider implements Deferrab
      */
     public function provides()
     {
-        return [StringBlade::class];
+        return [
+            StringBlade::class,
+            'view',
+            ViewFactory::class,
+        ];
     }
 }
