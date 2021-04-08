84251e57145e4ca1f53abc4fb17bb266de21469e
Jellyfrog <Jellyfrog@users.noreply.github.com>
2021-04-09 01:38:11 +0200

    Update phpdoc

--- src/Facades/StringBlade.php
+++ src/Facades/StringBlade.php
@@ -8,7 +8,7 @@ use Wpb\String_Blade_Compiler\Compilers\StringBladeCompiler;
 /**
  * @method static bool exists(string $view)
  * @method static \Illuminate\Contracts\View\View file(string $path, array $data = [], array $mergeData = [])
- * @method static \Illuminate\Contracts\View\View make(string $view, array $data = [], array $mergeData = [])
+ * @method static \Wpb\String_Blade_Compiler\StringView make(string|array $view, array $data = [], array $mergeData = [])
  * @method static mixed share(array|string $key, $value = null)
  * @method static array composer(array|string $views, \Closure|string $callback)
  * @method static array creator(array|string $views, \Closure|string $callback)
